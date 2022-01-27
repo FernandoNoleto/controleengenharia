3cf161c067254b8233165bcbf923eb31b7411c68
FernandoNoleto <fernando.barroso.noleto@gmail.com>
2022-01-27 18:47:52 -0300

    Faz calculo da media e mostra no modal criado

--- js/table.js
+++ js/table.js
@@ -1,4 +1,6 @@
 var tam = 0
+var values = [];
+
 
 function addRows() {
 
@@ -44,23 +46,64 @@ function submitValues() {
     }
 
 
-    var values = [];
-
     for (var i = 0; i <= elems.length - 4; i++) {
 
         if (elems[i].value == '') {
             $('#submitbtn').click(() => { ToastMaker(`Bloco #${i+1} está vazio`) });
             return;
         }
-        values[i] = elems[i].value;
+        values[i] = parseFloat(elems[i].value);
     }
 
-
-    document.getElementById("bloco1").innerHTML = values[0];
     console.log(values);
 
+
+    var media_text = document.getElementById("media").innerHTML;
+    var somatorio_text = document.getElementById("somatorio").innerHTML;
+    var desviopadrao_text = document.getElementById("desviopadrao").innerHTML;
+    var variacao_text = document.getElementById("variacao").innerHTML;
+    var fck_text = document.getElementById("fck").innerHTML;
+
+
+
+    document.getElementById("media").innerHTML = media_text + media();
+    document.getElementById("somatorio").innerHTML = somatorio_text + media();
+    document.getElementById("desviopadrao").innerHTML = desviopadrao_text + media();
+    document.getElementById("variacao").innerHTML = variacao_text + media();
+    document.getElementById("fck").innerHTML = fck_text + media();
 }
 
 function deleteRows() {
+
+}
+
+
+function media() {
+
+    var aux = 0;
+
+    for (let i = 0; i < values.length; i++) {
+        aux += values[i];
+        console.log(values[i]);
+    }
+
+
+    return (aux / values.length);
+}
+
+function somatorio() {
+
+}
+
+
+function desvio_padrao() {
+
+}
+
+function variacao() {
+
+}
+
+function fck() {
 
 }
